9fb4d4c51854ae4a94a7beb8bed95692a687a91a
polarbear0701 <phantnguyen2003@gmail.com>
2025-03-26 10:23:06 +0700

    chore: init

---
+++ PAWA_FE/src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+  return (
+    <div className='w-screen h-screen bg-amber-400'>
+        <p>Hello</p>
+        <button className="btn btn-error">Error</button>
+    </div>
+  )
+}
+
+export default App
